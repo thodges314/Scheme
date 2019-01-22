e3b86e9ec560b1defc774f82b1ddfcda5457a27a
Thomas Hodges <thodges@javascriptonline.org>
2019-01-22 13:19:37 -0800

    exponentiation work - later make iterative process for half-exp

---
+++ Chapter 1/exponentiation2.scm
@@ -0,0 +1,38 @@
+#lang scheme
+
+;; recursive exponentiation
+; crashes on 2 ^ 8,000,000 - 2 ^ 800,000 takes 1 minute 45 seconds
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+;(expt 2 800000)
+
+;; iterative exponentiation
+; tested for 2^8,000,000 - after 10 minutes killed the process - 2^800,000 takes 55 seconds
+
+(define (expt-i b n)
+  (define (expt-iter b counter product)
+    (if (= counter 0)
+        product
+        (expt-iter b (- counter 1) (* b product))))
+  (expt-iter b n 1))
+
+(expt-i 2 800000)
+
+; these are both too fast even with large numbers to detect time lags ^^^^
+
+;; fast-exp
+;; uses b^n = (b^(n/2))^2 is n is even, b^n = b * b^(n-1) if n is odd
+; finds 2^8,000,000 in about 4 seconds - finds 2^800,000 in less than 1 second
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (square x) (* x x))
+(define (even? n)
+    (= (remainder n 2) 0))
+
+;(fast-expt 2 800000)
